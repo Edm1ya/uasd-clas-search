9b3778ba4e68169a2126e338ac84168579a31951
Edward Minaya <ed.miya.14@gmail.com>
2024-08-04 09:26:47 -0400

    SubjectType added

---
+++ database/seeders/SubjectTypeSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SubjectType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SubjectTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        SubjectType::create([
+            'name' => 'lab'
+        ]);
+
+        SubjectType::create([
+            'name' => 'teo'
+        ]);
+    }
+}
